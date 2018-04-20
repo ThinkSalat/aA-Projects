147bac78ace8a8db95aa7e6d1cd06558bd7721aa
Shawn Salat <Shawn.Salat@gmail.com>
2018-04-20 13:20:30 -0700

    Add depth first search and breadth first search methods to PolyTreeNode class

--- W1D5/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ W1D5/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -5,6 +5,12 @@ class PolyTreeNode
     @children = []
   end
 
+  def inspect
+    # "#{@parent.value} is parent" if @parent
+    # "No parent!" unless @parent
+    "#{@value} #{@children.count} children"
+  end
+
   def parent
     @parent
   end
@@ -32,6 +38,25 @@ class PolyTreeNode
     raise "error" if !(@children.include?(node))
     node.parent = nil
     @children.delete(node)
+  end
+
+  def dfs(target)
+    return self if target == @value
+    @children.each do |child|
+      dfs_node = child.dfs(target)
+      return dfs_node unless dfs_node.nil?
+    end
+    nil
+  end
+
+  def bfs(target)
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target
+      queue.concat(node.children)
+    end
+    nil
   end
 
 end
