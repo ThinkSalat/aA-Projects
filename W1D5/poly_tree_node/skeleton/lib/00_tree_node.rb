44fc2caf94f1d2a5a32d7eb01d69454ecc33e8ed
Shawn Salat <Shawn.Salat@gmail.com>
2018-04-20 11:37:28 -0700

    Add #initialize to Node class, Add parent, parent=, and children to Node

--- W1D5/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ W1D5/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+  attr_accessor :value
+  def initialize(value)
+    @value = value
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def parent=(node)
+    @parent = node
+    @parent.children << self if @parent
+  end
+
+  def children
+    @children
+  end
 
 end
