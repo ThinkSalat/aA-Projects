ca719f433ff8bce14e2fe92fd6eaa3ebdbda42f1
Shawn Salat <Shawn.Salat@gmail.com>
2018-04-20 14:33:22 -0700

    Add @build_move_tree to KnightPathFinder class, builds Tree out of PolyTreeNode class nodes

--- W1D5/knights_travails/knightpathfinder.rb
+++ W1D5/knights_travails/knightpathfinder.rb
@@ -1,18 +1,19 @@
 require_relative 'polynodetree'
-DELTAS = [
-  [1,2],
-  [1,-2],
-  [-1,2],
-  [-1,-2],
-  [2,1],
-  [2,-1],
-  [-2,1],
-  [-2,-1]
-]
-class KnightPathFinder
 
+class KnightPathFinder
+  attr_accessor :visited_positions, :start_pos, :move_tree
+  DELTAS = [
+    [1,2],
+    [1,-2],
+    [-1,2],
+    [-1,-2],
+    [2,1],
+    [2,-1],
+    [-2,1],
+    [-2,-1]
+  ]
   def initialize(pos)
-    @position = pos
+    @start_pos = pos
     @visited_positions = [pos]
     build_move_tree
   end
@@ -21,28 +22,35 @@ class KnightPathFinder
     moves = DELTAS.map do |(dx,dy)|
       [pos[0] + dx, pos[1] + dy]
     end
-    moves.select! {|move| within_bounds?(move)}
+    moves.select {|move| KnightPathFinder.within_bounds?(move)}
   end
 
-  def within_bounds(pos)
+  def self.within_bounds?(pos)
     pos.all? {|coord| coord.between?(0,7)}
   end
 
-  def new_move_positions
-    new_moves = KnightPathFinder.valid_moves(@position).reject! { |move| @visited_positions.include?(move)}
-    @visited_positions.concat(new_moves)
+  def new_move_positions(pos)
+
+    new_moves = KnightPathFinder.valid_moves(pos)
+    new_moves.reject! { |move| @visited_positions.include?(move)}
+    new_moves.each {|move| @visited_positions << move}
     new_moves
   end
 
   def build_move_tree
-    root = @position
+    root = PolyTreeNode.new(@start_pos)
+    @move_tree = root
+    queue = [root]
+    until queue.empty?
+      this_node = queue.shift
+      new_move_positions(this_node.value).each { |pos| this_node.add_child(PolyTreeNode.new(pos)) }
+      queue.concat(this_node.children)
+    end
+    @move_tree
   end
 
+  
 
 end
 
-
-no moves out of bounds
-moves.select do |move|
-
-end
+k = KnightPathFinder.new([0,0])
