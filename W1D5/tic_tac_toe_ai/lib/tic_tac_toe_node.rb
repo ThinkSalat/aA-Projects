45c3f5d8a7798a291558d4927d549e497efdb32b
Shawn Salat <Shawn.Salat@gmail.com>
2018-04-20 16:48:25 -0700

    Add #losing_node? and #winning_node? methods to TicTacToeNode class These check if there's a possibility of losing or winning respectively

--- W1D5/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ W1D5/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,25 +1,29 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  #next_mover_mark = current player
   attr_accessor :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @board, @next_mover_mark = board, next_mover_mark
-    @prev_move_pos = prev_move_pos
+    @board, @next_mover_mark, @prev_move_pos =
+      board, next_mover_mark, prev_move_pos
   end
 
   def losing_node?(evaluator)
-    oppo_mark = (evaluator == :x) ? :o : :x
-    return true if @board.over? && @board.winner == oppo_mark
-    return false if @board.winner.nil? || @board.winner == evaluator
-
-    
+    return board.won? && board.winner != evaluator if board.over?
+    if next_mover_mark == evaluator
+      children.all? { |child| child.losing_node?(evaluator) }
+    else
+      children.any? { |child| child.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
-    oppo_mark = (evaluator == :x) ? :o : :x
-    return true if @board.over? && @board.winner == evaluator
-    return false if @board.winner.nil? || @board.winner == oppo_mark
-
+    return board.won? && board.winner == evaluator if board.over?
+    if next_mover_mark == evaluator
+      children.any? {|child| child.winning_node?(evaluator)}
+    else
+      children.all? {|child| child.winning_node?(evaluator)}
+    end
   end
 
   # This method generates an array of all moves that can be made after
